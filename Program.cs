cd54d4eed85aafacd4661a43158bd1de88aa5011
vuduythai1192k6 <vthat989@gmail.com>
2026-09-18 18:56:12 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap chuoi: ");
+        string chuoi = Console.ReadLine();
+
+        int dem = 0;
+        Console.Write("Nhap ky tu : ");
+        char a = Console.ReadLine()[0];
+        for (int i = 0; i < chuoi.Length; i++)
+        {
+            if (chuoi[i] == a)
+                dem++;
+        }
+        Console.Write("So lan xuat hien cua " + a + " la : " + dem);
+    }
+}
+
